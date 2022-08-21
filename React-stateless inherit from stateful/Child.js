f672e3ec81801359f627acefa844f1ac8251468f
Patrick Foster <94336659+patfost@users.noreply.github.com>
2022-08-21 13:20:20 +0700

    Parent, Child and Sibling

---
+++ React-stateless inherit from stateful/Child.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export class Child extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const name = e.target.value;
+    this.props.onChange(name);
+  }
+
+  render() {
+    return (
+      <div>
+        <select
+          id="great-names"
+          onChange={this.handleChange}>
+
+          <option value="Patrick">Patrick</option>
+          <option value="Jack">Jack</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </div>
+    );
+  }
+}
